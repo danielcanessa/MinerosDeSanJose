ad924c39e474a66ffd313effb3e83b1130e3224a
dcanessa <danielcanessa09@gmail.com>
2016-10-20 08:48:47 -0600

    ESP8266 left post

--- src/ESP8266/ESP8266.ino
+++ src/ESP8266/ESP8266.ino
@@ -3,94 +3,84 @@
  // arduino Tx (pin 3) ---- ESP8266 Rx
 SoftwareSerial esp8266(3,2); 
 
-const char* SSID = "LosMalekus";
-const char* PASSWORD = "morza200";
+String ssid = "dCanessa";
+String password = "edii5319";
+
 int resetPin=3;
-void setup()
+
+void initializeESP8266()
 {
-  
- Serial.begin(9600);  // monitor serial del arduino
-
-
- 
- esp8266.begin(9600); // baud rate del ESP8255
- 
- pinMode(13,OUTPUT);
- digitalWrite(13,LOW);
-
-// pinMode(resetPin,OUTPUT);
-// digitalWrite(resetPin,LOW);
-
- Serial.println("Initializing the connection");
+ esp8266.begin(115200); // baud rate del ESP8266
  sendData("AT+RST\r\n",2000);      // resetear módulo
  sendData("AT+CWMODE=1\r\n",1000); // configurar como cliente
- sendData("AT+CWJAP=\"LosMalekus\",\"morza200\"\r\n",8000); //SSID y contraseña para unirse a red 
+ sendData("AT+CWJAP=\""+ssid+"\",\""+password+"\"\r\n",8000); //SSID y contraseña para unirse a red 
  sendData("AT+CIFSR\r\n",1000);    // obtener dirección IP
  sendData("AT+CIPMUX=1\r\n",1000); // configurar para multiples conexiones
- sendData("AT+CIPSERVER=1,80\r\n",1000);         // servidor en el puerto 80
- Serial.println("Connection ready"); 
+ sendData("AT+CIPSERVER=1,80\r\n",1000);         // servidor en el puerto 80  
 }
 
-void loop()
+void handleESP8266()
 {
- if(esp8266.available())   // revisar si hay mensaje del ESP8266
+  if(esp8266.available())   // revisar si hay mensaje del ESP8266
  {
    if(esp8266.find("+IPD,")) // revisar si el servidor recibio datos
    {
-     delay(1500); // esperar que lleguen los datos hacia el buffer
+     delay(1000); // esperar que lleguen los datos hacia el buffer
      int conexionID = esp8266.read()-48; // obtener el ID de la conexión para poder responder
-     esp8266.find("led="); // bucar el texto "led="
-     int state = (esp8266.read()-48); // Obtener el estado del pin a mostrar
-     digitalWrite(13, state); // Cambiar estado del pin
 
-     Serial.println("Antes del while"); 
-     
-     while(esp8266.available())
+     int sendToServer=0;
+     if(esp8266.find("send"))
      {
-      Serial.println("Dentro del while"); 
+      sendToServer=1;
+      
+     }    
+          
+     while(esp8266.available())
+     {      
       char c = esp8266.read();
       Serial.print(c);
      }
+   
+     String webpage = "<h1>Mineros de San Jose</h1>";
 
-     Serial.println("Sali del while"); 
-     
-     //responder y cerrar la conexión para que el navegador no se quede cargando 
-     // página web a enviar
-     String webpage = "";
-     if (state==1)
+     if(sendToServer)
      {
-      webpage += "<h1>LED_13 = encendido!</h1>";
+       webpage+="<p>Enviando datos al server</p>";
+       
      }
-     else 
-     {
-      webpage += "<h1>LED_13 = apagado!</h1>";
-     }
-     
+        
      // comando para enviar página web
      String comandoWebpage = "AT+CIPSEND=";
      comandoWebpage+=conexionID;
      comandoWebpage+=",";
      comandoWebpage+=webpage.length();
      comandoWebpage+="\r\n";
-     Serial.println("Vienen prints: ");
-     Serial.println(comandoWebpage);
-     sendData(comandoWebpage,3000);
-     Serial.println(webpage);
-     sendData(webpage,3000);
      
-     // comando para terminar conexión
+     sendData(comandoWebpage,1500);
+   
+     sendData(webpage,1500);
+     
+     
      String comandoCerrar = "AT+CIPCLOSE=";
      comandoCerrar+=conexionID;
-     comandoCerrar+="\r\n";
-     Serial.println(comandoCerrar);
-     sendData(comandoCerrar,3000);
+     comandoCerrar+="\r\n";   
+     sendData(comandoCerrar,1500);
 
-    // digitalWrite(resetPin, LOW); // VCC +5V mode 
-    // delay(100);
-    // digitalWrite(resetPin, HIGH); // VCC +5V mode 
+    
    }
  }
 }
+void setup()
+{
+ Serial.begin(115200);  // monitor serial del arduino
+ initializeESP8266();
+}
+
+void loop()
+{
+  handleESP8266(); 
+}
+
 /*
 Enviar comando al esp8266 y verificar la respuesta del módulo, todo esto dentro del tiempo timeout
 */
@@ -102,10 +92,10 @@ void sendData(String comando, const int timeout)
  
  while( (time+timeout) > millis()) //mientras no haya timeout
  {
-   //Serial.println("Pegado while de afuera");
+
    while(esp8266.available()) //mientras haya datos por leer
    { 
-     //Serial.println("Pegado while de adentro");
+     
      // Leer los datos disponibles
      char c = esp8266.read(); // leer el siguiente caracter
      Serial.print(c);
